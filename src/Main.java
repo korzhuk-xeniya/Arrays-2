39827035e5878e34d42232e513261700295f4ce5
Ксения <korzhuk.xeniya@yandex.ru>
2023-05-26 23:14:05 +0300

    task-1

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
         task3();
         task4();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int arr = new int[30];
+        int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
@@ -20,6 +21,12 @@ public class Main {
         System.out.println("Массивы-2");
         System.out.println("Задача 1");
         int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int amountOfExpenses = 0;
+        for (int days = 0; days < arr.length; days++) {
+            amountOfExpenses = amountOfExpenses + arr[days];
+        }
+        System.out.println("Сумма трат за месяц составилв " + amountOfExpenses + " рублей");
 
 
     }
