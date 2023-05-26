46ceecbe4921ab963d43af316f9f27539a3d59fd
Ксения <korzhuk.xeniya@yandex.ru>
2023-05-26 22:52:20 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,20 @@ public class Main {
         task3();
         task4();
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
     public static void task1() {
         System.out.println("Массивы-2");
         System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+
 
     }
 
